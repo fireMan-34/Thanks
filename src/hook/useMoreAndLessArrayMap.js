1a21b53f17c005ff44667fb4f1be9e17c3a06a00
fireMan-34 <524915379@qq.com>
2022-05-29 14:01:58 +0800

    fix: 🐛 修复useCallback依赖项熟悉度不熟导致的错误,暂时删除useCallback。

--- src/hook/useMoreAndLessArrayMap.js
+++ src/hook/useMoreAndLessArrayMap.js
@@ -1,29 +1,26 @@
-import { useCallback, useEffect, useState } from "react"
+import {useState } from "react"
 
-export default function useMoreAndLessArrayMap(ListenArray,loadingCount){
-    if(!Array.isArray(ListenArray)&&typeof loadingCount !=="number"){
+export default function useMoreAndLessArrayMap(ListenArray, loadingCount) {
+    if (!Array.isArray(ListenArray) && typeof loadingCount !== "number") {
         throw new Error(`
         无错误处理
         `);
     }
-    const [pageCount,setPageCount]=useState(1);
-    const lessPage=useCallback(()=>{
-        if(pageCount>=1){
-            setPageCount(v=>v-1);
+    const [pageCount, setPageCount] = useState(1);
+    const lessPage = () => {
+        if (pageCount >= 1) {
+            setPageCount(v => v - 1);
         }
-    },[]);
-    const morePage=useCallback(()=>{
-        if(pageCount<Math.floor(ListenArray.length/pageCount)+1){
-            setPageCount(v=>v+1);
+    }
+    const morePage = () => {
+        if (pageCount < Math.floor(ListenArray.length / pageCount) + 1) {
             console.log(pageCount);
+            setPageCount(v => v + 1);
         }
-    },[]);
-    useEffect(()=>{
-        console.log(`render`,ListenArray.slice(0,loadingCount*pageCount));
-    },[JSON.stringify(ListenArray)]);
+    }
     return {
         lessPage,
         morePage,
-        data:ListenArray.slice(0,loadingCount*pageCount)||ListenArray,  
+        data: ListenArray.slice(0, loadingCount * pageCount) || ListenArray,
     }
 }
