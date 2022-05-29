2072b796e439325d4b42760629347e59ccbf1df5
fireMan-34 <524915379@qq.com>
2022-05-29 12:20:26 +0800

    fix: 🐛 修复英文阅读能力差导致的配置文件错误

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './store/index';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
